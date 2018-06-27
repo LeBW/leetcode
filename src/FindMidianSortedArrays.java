2cf04ddde3db42f2427473c921e34e9e4541eed9
LeBW <27091925@qq.com>
2018-06-28 00:19:20 +0800

    Improve FindMidianSortedArrays.

--- src/FindMidianSortedArrays.java
+++ src/FindMidianSortedArrays.java
@@ -12,38 +12,48 @@ public class FindMidianSortedArrays {
         int len1 = nums1.length, len2 = nums2.length;
         int mid = (len1 + len2) / 2;
         int p = 0, q = 0;
-        int cur = 0, pre = 0;
-        for (int i = 0; i <= mid; i++) {
+        int cur = 0;
+        for (int i = 0; i < mid; i++) {
             if (p >= len1) {
-                pre = cur;
-                cur = nums2[q++];
+                if (i == mid - 1)
+                    cur = nums2[q];
+                q++;
             }
             else if (q >= len2) {
-                pre = cur;
-                cur = nums1[p++];
+                if (i == mid - 1)
+                    cur = nums1[p];
+                p++;
             }
             else if (nums1[p] < nums2[q]) {
-                pre = cur;
-                cur = nums1[p++];
+                if (i == mid - 1)
+                    cur = nums1[p];
+                p++;
             }
             else {
-                pre = cur;
-                cur = nums2[q++];
+                if (i == mid - 1)
+                    cur = nums2[q];
+                q++;
             }
         }
-        if (p == 0 && q == 0)
-            return len1 < 1 ? nums2[q] : nums1[p];
-        System.out.println("cur: " + cur + " pre: " + pre + " mid: " + mid + " p: " + p + " q: " + q);
+        int next = 0;
+        if (p >= len1)
+            next = nums2[q];
+        else if (q >= len2)
+            next = nums1[p];
+        else if (nums1[p] < nums2[q])
+            next = nums1[p];
+        else
+            next = nums2[q];
         if ((len1 + len2) % 2 == 1)
-            return cur;
+            return next;
         else {
-            return (cur + pre) / 2.0;
+            return (cur + next) / 2.0;
         }
     }
 
     public static void main(String[] args) {
         FindMidianSortedArrays findMidianSortedArrays = new FindMidianSortedArrays();
-        double reuslt = findMidianSortedArrays.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4});
+        double reuslt = findMidianSortedArrays.findMedianSortedArrays(new int[]{1, 3}, new int[]{2});
         System.out.println(reuslt);
     }
 }
