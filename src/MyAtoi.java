b95310215e6c0b9ba9392eb0fadb2e23595873df
LeBW <27091925@qq.com>
2018-07-04 02:01:01 +0800

    Improve

--- src/MyAtoi.java
+++ src/MyAtoi.java
@@ -4,12 +4,12 @@
 public class MyAtoi {
     public int myAtoi(String str) {
         int len = str.length();
-
+        //judge whether str is empty.
         if (len <= 0)
             return 0;
 
         StringBuilder stringBuilder = new StringBuilder(str);
-        boolean negative = false;
+        boolean negative = false;  //whether the integer is positive or negative.
         int limit = -Integer.MAX_VALUE;
         int result = 0;
         int i = 0;
@@ -17,10 +17,11 @@ public class MyAtoi {
         //remove whitespace characters.
         while (stringBuilder.length() > 0 &&  stringBuilder.charAt(0) == ' ')
             stringBuilder.deleteCharAt(0);
-        //judge negative.
         len = stringBuilder.length();
-        if (len <= 0)
+        if (len <= 0) //judge whether str is all whitespace
             return 0;
+
+        //judge plus-minus sign.
         char c = stringBuilder.charAt(0);
         if (c < '0') {
             if (c == '-') {
@@ -33,9 +34,11 @@ public class MyAtoi {
                 return 0;
             i++;
         }
+        //accumulate the result
         for(; i < len; i++) {
             c = stringBuilder.charAt(i);
             if (Character.digit(c, 10) >= 0) {
+                //judge whether the value is going to overflow
                 if (result < (limit + Character.digit(c, 10)) / 10) {
                     return negative ? limit : -limit;
                 }
