b7df3ab6e0c73017a9e1a0a8ddcf3374c65c6f8a
LeBW <27091925@qq.com>
2018-07-18 00:19:30 +0800

    RemoveNthFromEnd

---
+++ src/RemoveNthFromEnd.java
@@ -0,0 +1,22 @@
+/**
+ * @author LBW
+ */
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode leader = head, cur = head;
+        for (int i = 0; i < n; i++)
+            leader = leader.next;
+        //if remove the first
+        if (leader == null) {
+            return head.next;
+        }
+        //go ahead, make cur.next be the node that is going to be deleted.
+        while (leader.next != null) {
+            leader = leader.next;
+            cur = cur.next;
+        }
+        //delete cur.next
+        cur.next = cur.next.next;
+        return head;
+    }
+}
