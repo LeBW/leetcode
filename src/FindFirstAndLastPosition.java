bc69580791dfc7453a49c147398b9ff8dd071cf4
LeBW <27091925@qq.com>
2018-12-20 19:49:57 +0800

    FindFirstAndLastPosition improved.

--- src/FindFirstAndLastPosition.java
+++ src/FindFirstAndLastPosition.java
@@ -6,10 +6,49 @@ public class FindFirstAndLastPosition {
         if (nums.length == 0)
             return new int[]{-1, -1};
 
-        int l = 0, r = nums.length - 1;
-        return find(nums, l, r, target);
+        int left = findLeftTarget(nums, 0, nums.length-1, target);
+        int right = -1;
+        if (left != -1)
+            right = findRightTarget(nums, 0, nums.length-1, target);
+        return new int[]{left, right};
     }
 
+    //A readable way.
+    private int findLeftTarget(int[] nums, int l, int r, int target) {
+        if (l > r || (l == r && nums[l] != target))
+            return -1;
+        else if (l == r) {
+            return l;
+        }
+        int mid = (l + r) / 2;
+        if (nums[mid] > target) {
+            return findLeftTarget(nums, l, mid-1, target);
+        }
+        else if (nums[mid] < target) {
+            return findLeftTarget(nums, mid+1, r, target);
+        }
+        else {
+            return findLeftTarget(nums, l, mid, target);
+        }
+    }
+    private int findRightTarget(int[] nums, int l, int r, int target) {
+        if (l > r || (l == r && nums[l] != target))
+            return -1;
+        else if (l == r)
+            return r;
+        int mid = (l + r + 1) / 2;
+        if (nums[mid] > target) {
+            return findRightTarget(nums, l, mid-1, target);
+        }
+        else if (nums[mid] < target) {
+            return findRightTarget(nums, mid+1, r, target);
+        }
+        else {
+            return findRightTarget(nums, mid, r, target);
+        }
+    }
+    //old ways.
+    @Deprecated
     private int[] find(int[] nums, int l, int r, int target) {
         if (l > r || (l==r && nums[l] != target))
             return new int[]{-1, -1};
@@ -29,6 +68,7 @@ public class FindFirstAndLastPosition {
             return new int[]{left, right};
         }
     }
+    @Deprecated
     private int findLeft(int[] nums, int l, int r, int target) {
         if (l == r)
             return r;
@@ -40,6 +80,7 @@ public class FindFirstAndLastPosition {
             return findLeft(nums, l, mid, target);
         }
     }
+    @Deprecated
     private int findRight(int[] nums, int l, int r, int target) {
         if (l >= r)
             return l;
@@ -51,4 +92,5 @@ public class FindFirstAndLastPosition {
             return findRight(nums, mid, r, target);
         }
     }
+
 }
