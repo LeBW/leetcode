eb378357cd7dcbfd94d01107ef840f607f173e54
LeBW <27091925@qq.com>
2020-06-24 09:10:16 +0800

    finish 222

--- src/CountCompleteTreeNodes.java
+++ src/CountCompleteTreeNodes.java
@@ -5,8 +5,33 @@
  * @author LBW
  */
 public class CountCompleteTreeNodes {
+    /**
+     * 判断左子树和右子树的高度。如果高度相同，说明左子树是满的；如果高度不同，说明右子树是满的。
+     */
     public int countNodes(TreeNode root) {
-        return dfs(root);
+        if (root == null)
+            return 0;
+
+        int left = countDepth(root.left);
+        int right = countDepth(root.right);
+        if (left == right) {
+            return countNodes(root.right) + (1 << (left+1));
+        }
+        else {
+            return countNodes(root.left) + (1 << (right+1));
+        }
+    }
+
+    private int countDepth(TreeNode root) {
+        if (root == null)
+            return -1;
+        int result = 0;
+        root = root.left;
+        while (root != null) {
+            root = root.left;
+            result += 1;
+        }
+        return result;
     }
 
     private int dfs(TreeNode root) {
