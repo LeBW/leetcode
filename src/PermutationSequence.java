1ea4b07b45342578da9cec146af6a86a97e014b3
LeBW <27091925@qq.com>
2020-08-26 13:30:31 +0800

    update 375. O(n^2)

--- src/PermutationSequence.java
+++ src/PermutationSequence.java
@@ -5,37 +5,40 @@
 public class PermutationSequence {
     private int idx;
     private String result;
+    private int[] factorial;
+
     public String getPermutation(int n, int k) {
         boolean[] isUsed = new boolean[n+1];
         StringBuilder cur = new StringBuilder();
         idx = 0;
+        // init factorial array
+        factorial = new int[10];
+        factorial[0] = 1;
+        for (int i = 1; i < 10; i++) {
+            factorial[i] = factorial[i-1] * i;
+        }
+        // DFS
         dfs(cur, isUsed, n, k);
         return result;
     }
 
-    private boolean dfs(StringBuilder cur, boolean[] isUsed, int n, int k) {
+    private void dfs(StringBuilder cur, boolean[] isUsed, int n, int k) {
         if (cur.length() == n) {
-            idx += 1;
-            if (idx == k) {
-                result = cur.toString();
-                return true;
-            }
-            else {
-                return false;
-            }
+            result = cur.toString();
+            return;
         }
+        int cnt = factorial[n - cur.length() - 1];
         for (int i = 1; i <= n; i++) {
             if (isUsed[i]) {
                 continue;
             }
+            if (cnt < k) {
+                k = k - cnt;
+                continue;
+            }
             cur.append(i);
             isUsed[i] = true;
-            if (dfs(cur, isUsed, n, k))
-                return true;
-            //false, backtrack
-            isUsed[i] = false;
-            cur.deleteCharAt(cur.length()-1);
+            dfs(cur, isUsed, n, k);
         }
-        return false;
     }
 }
