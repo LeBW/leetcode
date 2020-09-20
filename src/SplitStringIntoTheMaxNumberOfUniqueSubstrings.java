3620fb660e4f36f20cc4984f00533eb98488f1de
LeBW <27091925@qq.com>
2020-09-20 16:33:36 +0800

    modify 5520。回溯

--- src/SplitStringIntoTheMaxNumberOfUniqueSubstrings.java
+++ src/SplitStringIntoTheMaxNumberOfUniqueSubstrings.java
@@ -4,28 +4,34 @@ import java.util.Set;
 
 /**
  * 5520. Split a String Into the Max Number of Unique Substrings
+ * 回溯法。
  * @author LBW
  */
 public class SplitStringIntoTheMaxNumberOfUniqueSubstrings {
+    private int res;
+    private Set<String> set;
     public int maxUniqueSplit(String s) {
-        Set<String> set = new HashSet<>();
-        int beginIdx = 0, prev = 0, cur = 0, res = 0;
-        while (beginIdx != s.length()) {
-            if (!set.contains(s.substring(beginIdx, cur+1))) {
-                set.add(s.substring(beginIdx, cur+1));
-                prev = beginIdx;
-                beginIdx = cur + 1;
-                res += 1;
-            }
-            cur += 1;
-            if (cur == s.length() && beginIdx != s.length()) {
-                //回退
-                cur = beginIdx - 1;
-                beginIdx = prev;
-                res -= 1;
+        res = 0;
+        set = new HashSet<>();
+        backTrack(s, 0);
+        return res;
+    }
+
+    private void backTrack(String s, int idx) {
+        if (idx == s.length()) {
+            res = Math.max(res, set.size());
+            return;
+        }
+
+        if (s.length() - idx + set.size() <= res)// 剪枝. 思路就是：假设当前后面所有的字符都能组成单独的字符串，如果大小还是不能超过res，就不用尝试了。
+            return;
+        for (int i = idx + 1; i <= s.length(); i++) {
+            if (!set.contains(s.substring(idx, i))) {
+                set.add(s.substring(idx, i));
+                backTrack(s, i);
+                set.remove(s.substring(idx, i));
             }
         }
-        return res;
     }
 
     public static void main(String[] args) {
