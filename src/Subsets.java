8c5330c2d9349009547bf36a2341d2258a180b7a
LeBW <27091925@qq.com>
2020-09-20 23:05:03 +0800

    modify 78,回溯

--- src/Subsets.java
+++ src/Subsets.java
@@ -24,4 +24,28 @@ public class Subsets {
         }
         return result;
     }
+
+    /**
+     * 方法二：回溯法。将其想象为一棵n叉树，每个节点都可以选择或者不选。
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsetsII(int[] nums) {
+        // backTrack
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        backTrack(nums, 0, cur, res);
+        return res;
+    }
+
+    private void backTrack(int[] nums, int idx, List<Integer> cur, List<List<Integer>> res) {
+        res.add(new ArrayList<>(cur));
+        if (idx == nums.length)
+            return;
+        for (int i = idx; i < nums.length; i++) {
+            cur.add(nums[i]);
+            backTrack(nums, i + 1, cur, res);
+            cur.remove(cur.size() - 1);
+        }
+    }
 }
