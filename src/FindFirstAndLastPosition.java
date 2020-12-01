4200c915a0fe95530f9be34cbee1f7e65e66323b
LeBW <27091925@qq.com>
2020-12-01 16:12:14 +0800

    improve 34.二分查找

--- src/FindFirstAndLastPosition.java
+++ src/FindFirstAndLastPosition.java
@@ -1,4 +1,9 @@
 /**
+ * 34. Find first and Last Position of Element in Sorted Array.
+ * 二分查找。细节满满。
+ * 需要注意，在二分法逼近的时候，分两种情况：
+ * 1. left = mid+1, right = mid. 这种逼近的时候，rihgt不会主动向左逼近，为了防止死循环，mid需要向下取整（偏左）。
+ * 2. left = mid, right = mid - 1. 这种逼近的时候，left不会主动向右逼近，为了防止死循环，mid需要向上取整（偏右）。
  * @author LBW
  */
 public class FindFirstAndLastPosition {
@@ -6,14 +11,63 @@ public class FindFirstAndLastPosition {
         if (nums.length == 0)
             return new int[]{-1, -1};
 
-        int left = findLeftTarget(nums, 0, nums.length-1, target);
-        int right = -1;
-        if (left != -1)
-            right = findRightTarget(nums, 0, nums.length-1, target);
+        int left = findLeft(nums, target);
+        if (left == -1)
+            return new int[]{-1, -1};
+        int right = findRight(nums, target);
         return new int[]{left, right};
     }
 
-    //A readable way.
+    private int findLeft(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0)
+            return -1;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else if (nums[mid] == target) {
+                right = mid;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        if (nums[left] == target)
+            return left;
+        return -1;
+    }
+
+    private int findRight(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            }
+            else if (nums[mid] == target) {
+                left = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        if (nums[left] == target)
+            return left;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 7, 7, 8, 8, 10};
+        FindFirstAndLastPosition findFirstAndLastPosition = new FindFirstAndLastPosition();
+        findFirstAndLastPosition.searchRange(nums, 8);
+    }
+
+
+    //老办法。递归。
     private int findLeftTarget(int[] nums, int l, int r, int target) {
         if (l > r || (l == r && nums[l] != target))
             return -1;
@@ -47,50 +101,4 @@ public class FindFirstAndLastPosition {
             return findRightTarget(nums, mid, r, target);
         }
     }
-    //old ways.
-    @Deprecated
-    private int[] find(int[] nums, int l, int r, int target) {
-        if (l > r || (l==r && nums[l] != target))
-            return new int[]{-1, -1};
-        else if (l == r && nums[l] == target) {
-            return new int[]{l, l};
-        }
-        int mid = (l + r) / 2;
-        if (nums[mid] > target) {
-            return find(nums, l, mid - 1, target);
-        }
-        else if (nums[mid] < target) {
-            return find(nums, mid + 1, r, target);
-        }
-        else {
-            int left = findLeft(nums, l, mid, target);
-            int right = findRight(nums, mid, r, target);
-            return new int[]{left, right};
-        }
-    }
-    @Deprecated
-    private int findLeft(int[] nums, int l, int r, int target) {
-        if (l == r)
-            return r;
-        int mid = (l + r) / 2;
-        if (nums[mid] < target) {
-            return findLeft(nums, mid + 1, r, target);
-        }
-        else {
-            return findLeft(nums, l, mid, target);
-        }
-    }
-    @Deprecated
-    private int findRight(int[] nums, int l, int r, int target) {
-        if (l >= r)
-            return l;
-        int mid = (l + r + 1) / 2;
-        if (nums[mid] > target) {
-            return findRight(nums, l, mid - 1, target);
-        }
-        else {
-            return findRight(nums, mid, r, target);
-        }
-    }
-
 }
