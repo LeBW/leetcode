149c7255f754c9d5b34bb8f2af20d3bdac807b36
LeBW <27091925@qq.com>
2021-02-08 12:03:00 +0800

    finish 279

--- src/PerfectSquares.java
+++ src/PerfectSquares.java
@@ -16,8 +16,11 @@ public class PerfectSquares {
         for (int i = 2; i <= n; i++) {
             if (dp[i] == 1)
                 continue;
-            for (int j = 1; j <= i / 2; j++) {
-                dp[i] = Math.min(dp[i], dp[j] + dp[i-j]);
+//            for (int j = 1; j <= i / 2; j++) {
+//                dp[i] = Math.min(dp[i], dp[j] + dp[i-j]);  // 这种解法时间复杂度很高，为O(n^2)。其实这里有很多重复，可以更精简的遍历，利用完全平方数
+//            }
+            for (int j = 1; j * j < i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
             }
         }
         return dp[n];
