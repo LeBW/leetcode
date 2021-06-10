4e323605b5f544901e224e092178717df293d29d
LeBW <27091925@qq.com>
2021-06-10 10:24:15 +0800

    finish Offer14 - 2，用了大数 BigInteger

--- src/lcof/Offer14.java
+++ src/lcof/Offer14.java
@@ -1,8 +1,11 @@
 package lcof;
 
+import java.math.BigInteger;
+
 /**
  * Offer14. 剪绳子
- * 动态规划。时间复杂度 O(n^2)
+ * 动态规划。时间复杂度 O(n^2)\
+ * 注意，这里利用了 BigInteger，因为 long 也不够。。
  * @author LBW
  */
 public class Offer14 {
@@ -11,20 +14,23 @@ public class Offer14 {
             return 1;
         if (n == 3)
             return 2;
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
+        BigInteger[] dp = new BigInteger[n+1];
+        dp[0] = BigInteger.valueOf(0);
+        dp[1] = BigInteger.valueOf(1);
+        dp[2] = BigInteger.valueOf(2);
+        dp[3] = BigInteger.valueOf(3);
+        dp[4] = BigInteger.valueOf(4);
+        for (int i = 5; i <= n; i++) {
+            dp[i] = BigInteger.valueOf(i);
             for (int j = 1; j < i; j++) {
-                dp[i] = Math.max(dp[i], j * dp[i - j]);
+                dp[i] = dp[i].max(dp[i - j].multiply(BigInteger.valueOf(j)));
             }
         }
-        return dp[n];
+        return dp[n].mod(BigInteger.valueOf(1000000007)).intValue();
     }
 
     public static void main(String[] args) {
         Offer14 offer14 = new Offer14();
-        System.out.println(offer14.cuttingRope(3));
+        System.out.println(offer14.cuttingRope(120));
     }
 }
