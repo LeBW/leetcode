d419c403dc99806a71e404afb52a179acd1c5046
LeBW <27091925@qq.com>
2021-08-19 09:47:43 +0800

    finish 18

--- src/FourSum.java
+++ src/FourSum.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 /**
  * <a href=https://leetcode.com/problems/4sum/description/>
- * Click here to see online description.
+ * 18. Four Sum
+ * 两种方法：1. 回溯，时间复杂度O(n^4)。2. 类似三数之和，先排序，然后两层遍历 + 一层双指针，时间复杂度 O（n^3）
  * </a>
  *<br>
- * Like 3Sum. Time complexity: O(n^3)
- *
  * @author LBW
  */
 public class FourSum {
+    /**
+     * 类似三数之和，先排序，然后两层遍历 + 一层双指针。时间复杂度 O(n^3)。
+     */
     public List<List<Integer>> fourSum(int[] nums, int target) {
         //sort the nums array.
         Arrays.sort(nums);
@@ -50,5 +52,36 @@ public class FourSum {
             }
         }
         return results;
+    }
+
+    /**
+     * 排序 + 回溯。时间复杂度 O(n^4)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> fourSumTwo(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        backTrack(nums, 0, cur, target, res);
+        return res;
+    }
+
+    private void backTrack(int[] nums, int idx, List<Integer> cur, int target, List<List<Integer>> res) {
+        if (cur.size() == 4) {
+            if (target == 0) {
+                res.add(new ArrayList<>(cur));
+            }
+            return;
+        }
+        for (int i = idx; i < nums.length; i++) {
+            if (i > idx && nums[i] == nums[i -1]) {
+                continue;
+            }
+            cur.add(nums[i]);
+            backTrack(nums, i + 1, cur, target - nums[i], res);
+            cur.remove(cur.size() - 1);
+        }
     }
 }
