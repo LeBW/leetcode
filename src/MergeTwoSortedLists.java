75a60749c07d597430105c0c7b85fe4fb995ad77
Bowen Li <27091925@qq.com>
2021-09-18 13:36:14 +0800

    review 21,23

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -1,4 +1,5 @@
 /**
+ * 21. Merge Two Sorted Lists
  * <a href=https://leetcode-cn.com/problems/merge-two-sorted-lists/description/>
  * Click here to see online description.
  * </a>
@@ -8,25 +9,25 @@
  */
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode(-1);
         ListNode cur = dummy;
         while (l1 != null && l2 != null) {
             if (l1.val <= l2.val) {
-                cur.next = new ListNode(l1.val);
-                cur = cur.next;
+                cur.next = l1;
                 l1 = l1.next;
             }
             else {
-                cur.next = new ListNode(l2.val);
-                cur = cur.next;
+                cur.next = l2;
                 l2 = l2.next;
             }
+            cur = cur.next;
         }
         if (l1 != null) {
             cur.next = l1;
         }
-        if (l2 != null)
+        else if (l2 != null) {
             cur.next = l2;
+        }
         return dummy.next;
     }
 }
