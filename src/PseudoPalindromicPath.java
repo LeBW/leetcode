93890f0d5ce3b9f3fe7c6ce54c10ad0e2161fb1e
Petrov Makariy <makariyp@icloud.com>
2023-05-05 15:56:57 +0300

    Update PseudoPalindromicPath.java
    Intuition
    To begin with, let's think about how to determine that a set of numbers is a palindrome? It's very simple: in a set of numbers, only one element may not have a pair, all the others must have a pair.
    
    Approach
    So I decided to override the add method in ArrayList and check if there is an item in the list that we want to add. If such an element already exists, we will delete it. If there are 1 or 0 elements left in the ArrayList at the end, then all the numbers that we added to it earlier are a palindrome

--- src/PseudoPalindromicPath.java
+++ src/PseudoPalindromicPath.java
@@ -8,45 +8,33 @@ import java.util.Map;
  * @author LBW
  */
 public class PseudoPalindromicPath {
-    private int result = 0;
-    public int pseudoPalindromicPaths (TreeNode root) {
-        ArrayList<Integer> path = new ArrayList<>();
-        result = 0;
-        dfs(root, path);
-
-        return result;
-    }
-    private void dfs(TreeNode root, ArrayList<Integer> path) {
-        path.add(root.val);
-        if (root.left == null && root.right == null) {  //到叶节点了，path已经构建完毕，可以判断是否为回文序列了。
-            if (isPseudoPalindrome(path))
-                result += 1;
+    
+    static class MyList extends ArrayList<Integer> {
+        @Override
+        public boolean add(Integer integer) {
+            if (contains(integer)) {
+                remove(integer);
+                return true;
+            }
+            return super.add(integer);
         }
-        if (root.left != null)
-            dfs(root.left, new ArrayList<>(path));
-        if (root.right != null)
-            dfs(root.right, new ArrayList<>(path));
+    }
+    
+    public int pseudoPalindromicPaths(TreeNode root) {
+        return getCount(new MyList(), root);
     }
 
-    private boolean isPseudoPalindrome(ArrayList<Integer> path) {
-        Hashtable<Integer, Integer> hashtable = new Hashtable<>();
-
-        for (Integer i: path) {
-            if (!hashtable.containsKey(i)) {
-                hashtable.put(i, 1);
-            }
-            else {
-                hashtable.put(i, hashtable.get(i)+1);
-            }
+    private int getCount(MyList list, TreeNode root) {
+        if (root == null) return 0;
+    
+        list.add(root.val);
+    
+        if (root.left == null && root.right == null) {
+            return list.size() > 1 ? 0 : 1;
+        } else {
+            return getCount((MyList) list.clone(), root.left) + 
+                   getCount((MyList) list.clone(), root.right);
         }
-        int oddCount = 0;
-        for (Map.Entry<Integer, Integer> entry: hashtable.entrySet()) {
-            if (entry.getValue() % 2 == 1)
-                oddCount += 1;
-            if (oddCount > 1)
-                return false;
-        }
-        return true;
     }
 
     public static void main(String[] args) {
