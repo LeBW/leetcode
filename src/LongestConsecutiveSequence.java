5fa54163d03458474845f7aa3b32bc3e579b0b89
LeBW <27091925@qq.com>
2024-05-05 18:47:58 +0800

    redo 128

--- src/LongestConsecutiveSequence.java
+++ src/LongestConsecutiveSequence.java
@@ -3,27 +3,30 @@ import java.util.Set;
 
 /**
  * 128. Longest Consecutive Sequence
+ * 用到了哈希集，思路很难想到
  *
  * @author LBW
  */
 public class LongestConsecutiveSequence {
     public int longestConsecutive(int[] nums) {
+        // 先用 set 存储所有数
         Set<Integer> set = new HashSet<>();
         for (int num: nums) {
             set.add(num);
         }
-        int res = 0;
-        for (int num: nums) {
-            if (set.contains(num - 1)) {  //  说明 num 不是序列中的第一个元素，可以跳过
+        // 循环计算每个数字开头的最长连续序列的长度，取最大值
+        int max = 0;
+        for (int num: set) {
+            if (set.contains(num - 1)) {
                 continue;
             }
-            int curLength = 1;
-            while (set.contains(num + 1)) {
-                curLength += 1;
-                num += 1;
+            int cur = num;
+            while (set.contains(cur + 1)) {
+                cur += 1;
             }
-            res = Math.max(res, curLength);
+            max = Math.max(max, cur - num + 1);
         }
-        return res;
+        return max;
     }
+
 }
