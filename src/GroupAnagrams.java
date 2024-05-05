4460c3628ae2014fe36f8478fb92ebdd75a7e414
LeBW <27091925@qq.com>
2024-05-05 10:53:26 +0800

    redo 49

--- src/GroupAnagrams.java
+++ src/GroupAnagrams.java
@@ -19,9 +19,27 @@ public class GroupAnagrams {
         return new ArrayList<>(map.values());
     }
 
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str: strs) {
+            char[] cs = str.toCharArray();
+            Arrays.sort(cs);
+            // 这里不能用 cs.toString() ，否则 key 会无效！
+            String key = String.valueOf(cs);
+            if (map.containsKey(key)) {
+                map.get(key).add(str);
+            } else {
+                List<String> l = new ArrayList<>();
+                l.add(str);
+                map.put(key, l);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+
     public static void main(String[] args) {
         GroupAnagrams groupAnagrams = new GroupAnagrams();
-        List<List<String>> list = groupAnagrams.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+        List<List<String>> list = groupAnagrams.groupAnagrams2(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
         System.out.println(list);
     }
 }
